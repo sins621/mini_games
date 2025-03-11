b62cc49b89525f26fff301e04475b5a36ea3e13d
Sins_621 <sinsmailza@gmail.com>
2025-03-11 17:42:00 +0200

    add/hosting

---
+++ hosting/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/pong", express.static(path.join(__dirname, "public", "pong")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
