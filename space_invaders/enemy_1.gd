be323159aeffcbd62d2e6be4a9f73dd35b573dc4
Sins_621 <sinsmailza@gmail.com>
2025-04-23 22:15:08 +0200

    feat/enemies-turning

--- space_invaders/enemy_1.gd
+++ space_invaders/enemy_1.gd
@@ -1,8 +1,24 @@
 extends CharacterBody2D
 
 @export var sprite: Sprite2D
-@export var timer: Timer
 
-func _on_timer_timeout() -> void:
+signal out_of_bounds
+
+var direction = 10
+var parent: Node2D
+
+func _ready():
+	parent = get_parent()
+	parent.connect("tick", _on_tick)
+	parent.connect("change_direction", _on_change_direction)
+
+func _process(_delta: float) -> void:
+	if self.position.x > get_viewport_rect().size.x or self.position.x < 0:
+		out_of_bounds.emit()
+
+func _on_tick():
 	sprite.frame = 1 if sprite.frame == 0 else 0
-	timer.start()
+	self.position.x += direction
+
+func _on_change_direction():
+	direction *= -1
