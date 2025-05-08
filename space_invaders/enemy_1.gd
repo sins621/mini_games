b2f8f84194cf7700d5c9d8091d91a344f0963499
sins621 <sinsmailza@gmail.com>
2025-05-08 19:20:27 +0200

    update/remove-old-movement

--- space_invaders/enemy_1.gd
+++ space_invaders/enemy_1.gd
@@ -2,8 +2,6 @@ extends CharacterBody2D
 
 @export var sprite: Sprite2D
 
-signal out_of_bounds
-
 var direction = 10
 var parent: Node2D
 var down_distance = 10
@@ -18,39 +16,14 @@ var can_change_state = true
 func _ready():
 	parent = get_parent()
 	parent.connect("tick", _on_tick)
-	parent.connect("change_direction", _on_change_direction)
 	screen_size = get_viewport_rect().size
 	debug_label = parent.get_node("%Debug")
-	debug_label.text = "Debug\n" + "State: " + str(current_state) + "\nDirection: " + str(direction) + "\n can_change_state: " + str(can_change_state)
  
 func _on_tick():
-	debug_label.text = "Debug\n" + "State: " + str(current_state) + "\nDirection: " + str(direction) + "\n can_change_state: " + str(can_change_state)
-	
 	if current_state != State.DYING and sprite.frame == 0:
 		sprite.frame = 1
 	elif current_state != State.DYING and sprite.frame == 1:
 		sprite.frame = 0
-	
-	if current_state == State.MOVING:
-		global_position.x += direction
-		if not can_change_state:
-			can_change_state = true
-	elif current_state == State.CHANGING_DIRECTION:
-		direction *= -1
-		current_state = State.MOVING_DOWN
-	elif current_state == State.MOVING_DOWN:
-		global_position.y += down_distance
-		current_state = State.MOVING
-	
-	if global_position.x > screen_size.x:
-		out_of_bounds.emit()
-	if global_position.x < 0:
-		out_of_bounds.emit()
-
-func _on_change_direction():
-	if can_change_state:
-		current_state = State.CHANGING_DIRECTION
-		can_change_state = false
 
 func _on_hurtbox_body_entered(body: Node2D) -> void:
 	if body.name != "Projectile":
--- space_invaders/enemy_3.gd
+++ space_invaders/enemy_3.gd
@@ -3,12 +3,8 @@ extends CharacterBody2D
 @export var sprite: Sprite2D
 @export var projectile_scene: PackedScene
 
-signal out_of_bounds
-
 var direction = 10
 var parent: Node2D
-var should_move_down = false
-var should_change_direction = false
 var down_distance = 10
 var bounds_offset = 20
 var projectile = null
@@ -17,45 +13,21 @@ var dying = false
 func _ready():
 	parent = get_parent()
 	parent.connect("tick", _on_tick)
-	parent.connect("change_direction", _on_change_direction)
-
-func _process(_delta):
-	if self.position.x > get_viewport_rect().size.x - bounds_offset or self.position.x < bounds_offset:
-		out_of_bounds.emit()
-	if projectile and projectile.position.x > get_viewport_rect().size.x:
-		projectile.queue_free()
-		projectile = null
 
 func _on_tick():
 	if !dying and sprite.frame == 0:
 		sprite.frame = 1
 	elif !dying and sprite.frame == 1:
 		sprite.frame = 0
-	if should_move_down:
-		self.position.y += 1 * down_distance
-		should_move_down = false
-	else:
-		should_change_direction = true
-		self.position.x += direction
-	if randi() % 20 == 0 and !projectile:
-		projectile = projectile_scene.instantiate()
-		projectile.global_position = self.global_position
-		parent.add_child(projectile)
-
-func _on_change_direction():
-	if should_change_direction:
-		should_move_down = true
-		should_change_direction = false
-		direction *= -1
 
 func _on_hurtbox_body_entered(body: Node2D) -> void:
-		if body.name != "Projectile":
-			return
-		dying = true
-		direction = 0
-		sprite.frame = 2
-		parent.add_score(40)
-		body.queue_free()
-		$Hurtbox/CollisionShape2D.set_deferred("disabled", true)
-		await get_tree().create_timer(0.5).timeout
-		self.queue_free()
+	if body.name != "Projectile":
+		return
+	dying = true
+	direction = 0
+	sprite.frame = 2
+	parent.add_score(40)
+	body.queue_free()
+	$Hurtbox/CollisionShape2D.set_deferred("disabled", true)
+	await get_tree().create_timer(0.5).timeout
+	self.queue_free()
