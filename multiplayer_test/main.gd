09f0b03cee338e20d3afa5e421906ce28d444499
Sins_621 <sinsmailza@gmail.com>
2025-05-09 22:53:04 +0200

    update: websockets did not work

--- multiplayer_test/main.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-# Example: Load room ID from URL
-
-func _ready() -> void:
-	var room_id = JavaScriptBridge.eval("window.location.href")
-	print(room_id)
-	#JavaScriptBridge.eval("print('hello')")
