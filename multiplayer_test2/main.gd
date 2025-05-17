8c04e1386819b0cf9d1463e47637efca8a00f27c
sins621 <sinsmailza@gmail.com>
2025-05-17 23:35:45 +0200

    refactor: remove whitespace

--- multiplayer_test2/main.gd
+++ multiplayer_test2/main.gd
@@ -5,7 +5,6 @@ func send_message_to_server(msg: String):
 	if multiplayer.is_server():
 		print("Server received message from peer %s: %s" % [multiplayer.get_remote_sender_id(), msg])
 
-
 func _ready():
 	if OS.has_feature("dedicated_server"):
 		print("Running as server")
