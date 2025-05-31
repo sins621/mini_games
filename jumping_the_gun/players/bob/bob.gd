4a14ee5f6ee0ff43799a395b40b669461c629293
sins621 <sinsmailza@gmail.com>
2025-06-01 00:13:31 +0200

    feat: fix player movement

--- jumping_the_gun/players/bob/bob.gd
+++ jumping_the_gun/players/bob/bob.gd
@@ -1,39 +1,48 @@
 extends CharacterBody2D
 
-@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var marker: Marker2D = $Marker2D
-@onready var bullet: PackedScene = preload("res://entities/bullets/bullet.tscn")
+const SPEED : int = 100
+const JUMP_VELOCITY = -150
 
-var state
-var states = {}
+var GRAVITY : int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-const DEFAULT_GRAVITY = 700.0
+@onready var sprite : AnimatedSprite2D = $ViewContainer/AnimatedSprite2D
+@onready var ViewContainer : Node2D = $ViewContainer
+@onready var bullet_spawn : Marker2D = $ViewContainer/Marker2D
+@onready var bullet = preload("res://entities/bullets/bullet.tscn")
 
-func _ready():
-	states = {
-		"idle": preload("./states/idle.gd").new(),
-		"move": preload("./states/run.gd").new(),
-		"jump": preload("./states/jump.gd").new()
-	}
-	change_state("idle")
+func _ready() -> void:
+	pass
 
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("left"):
+		velocity.x = -SPEED
+	elif Input.is_action_pressed("right"):
+		velocity.x = SPEED
+	else:
+		velocity.x = 0
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
 	if not is_on_floor():
-		velocity.y += DEFAULT_GRAVITY * delta
-
-func _process(delta):
-	state.update(self, delta)
-	move_and_slide()
+		velocity.y += GRAVITY * delta
+	
+	if not is_on_floor():
+		sprite.play("jump")
+	else:
+		if velocity:
+			sprite.play("run")
+		else:
+			sprite.play("idle")
+	
+	if velocity.x > 0:
+		ViewContainer.scale.x = 1
+	elif velocity.x < 0:
+		ViewContainer.scale.x = -1
+	
 	if Input.is_action_just_pressed("shoot"):
-		var bullet_instance = bullet.instantiate()
-		bullet_instance.position = marker.global_position
-		get_tree().current_scene.add_child(bullet_instance)
-
-func change_state(new_state: String):
-	if state and state.exit:
-		state.exit(self)
+		var new_bullet: CharacterBody2D = bullet.instantiate()
+		new_bullet.global_position = bullet_spawn.global_position
+		get_parent().add_child(new_bullet)
 	
-	state = states[new_state]
-	
-	if state.enter:
-		state.enter(self)
+	move_and_slide()
